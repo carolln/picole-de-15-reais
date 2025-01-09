ac09932da9f5c4d6ac1572aaea9cbe0b52356eaf
carol <carolina.nunes.110@ufrn.edu.br>
2025-01-09 20:02:34 -0300

    Co-authored-by: Arthur José <ArthurJoseAS@users.noreply.github.com>

--- neander.cpp
+++ neander.cpp
@@ -9,30 +9,68 @@ using std::string;
 using std::map;
 
 
-struct REG{
-
+class REG{
+    public:
     std::string r;
 
+    string return_reg() {
+        return r;
+    }
+
+    void receive(string a) {
+        r = a;
+    }
+
 };
 
-struct MEM{
+class MEM{
+    public:
     
     std::string M[256];
+
+
+    string read(int idx) {
+        return M[idx];
+    }
+
+    void write(string s, int idx) {
+        M[idx] = s;
+    }
     
     
 };
 
-struct REM{
+class REM{
 
-    void sta(int idx, std::string a) {
-        //memoria.M[idx] = a;
+    string rem;
+    
+    public:
+    void str(std::string a) {
+        rem = a;
     }
 };
 
-struct RDM{};
+class RDM{
 
-struct PC{
+    string rdm;
     
+    public:
+    std::string read(MEM m, int idx) {
+        m.read(idx);
+    }
+
+    void write(MEM m, string s, int idx) {
+        m.write(s, idx);
+    }
+
+    string return_rdm() {
+        return rdm;
+    }
+    
+};
+
+class PC{
+    public:
     int p=0; 
 
     void increment() {
@@ -43,32 +81,37 @@ struct PC{
         p = n;
     }
 
+    int return_p() {
+        return p;
+    }
+
 };
 
-struct ULA{
-
+class ULA{
+    public:
     void add(REG &RA, int mem_val) {
-        int a = std::stoi(RA.r);
-        RA.r = std::to_string(a+mem_val);
+        int a = std::stoi(RA.return_reg());
+        RA.receive(std::to_string(a+mem_val));
     }
 
     void sub(REG &RA, int mem_val) {
-        int a = std::stoi(RA.r);
-        RA.r = std::to_string(a-mem_val);
+        int a = std::stoi(RA.return_reg());
+        RA.receive(std::to_string(a-mem_val));
     }
 
     void andy(REG &RA, int mem_val) {
-        int a = std::stoi(RA.r);
-        RA.r = std::to_string(a&mem_val);   
+        int a = std::stoi(RA.return_reg());
+        RA.receive(std::to_string(a&mem_val));   
     }
+    
     void ore(REG &RA, int mem_val) {
-        int a = std::stoi(RA.r);
-        RA.r = std::to_string(a|mem_val); 
+        int a = std::stoi(RA.return_reg());
+        RA.receive(std::to_string(a|mem_val)); 
     }
 
     void note(REG &RA) {
-        int a = std::stoi(RA.r);
-        RA.r = std::to_string(~a);
+        int a = std::stoi(RA.return_reg());
+        RA.receive(std::to_string(~a));
     }
 
 };
@@ -92,85 +135,196 @@ std::vector<std::string> tokenize (std::string line) {
 
 
 class Neander{
-
+    public:
     enum states {
         busca,
         execucao
     };
+    class PO{
+        public:
+        ULA unidade_arit;
+        MEM memoria;
+        RDM readmem;
+        REM rem;
+        PC program_count;
+        REG ac;
+    };
+    class UC{
+        enum sinais_controle{
+            cargaRI=0,
+            cargaNZ,
+            cargaRDM,
+            cargaREM,
+            cargaPC,
+            incrementaPC,
+            cargaAC,
+            selUAL,
+            sel,
+            read,
+            write
+            
+        };
 
+        sinais_controle sc;
+    class MUX{
+        
+    };
+
+
+        void ciclo_de_busca (PO parte_operativa) {
+
+            switch (sc)
+            {
+            case sinais_controle::incrementaPC:
+                parte_operativa.program_count.increment();
+                sc = sinais_controle::sel;
+                break;
+            
+            case sinais_controle::sel:
+            // wtffffff how are we gonna do this
+                parte_operativa.program_count.increment();
+                sc = sinais_controle::sel;
+                break;
+            
+            case sinais_controle::cargaREM:
+                parte_operativa.rem.str(std::to_string(parte_operativa.program_count.return_p()));
+                sc = sinais_controle::sel;
+                break;
+            
+            case sinais_controle::read:
+                //parte_operativa.
+                //sc = sinais_controle::sel;
+                break;
+            
+            case sinais_controle::write:
+                //string a = parte_operativa.program_count.return_p()
+                parte_operativa.memoria.write(parte_operativa.readmem.return_rdm());
+                sc = sinais_controle::sel;
+                break;
+            
+            case sinais_controle::cargaRDM:
+                parte_operativa.rem.str();
+                sc = sinais_controle::sel;
+                break;
+
+            case sinais_controle::cargaRI:
+                parte_operativa.rem.str();
+                sc = sinais_controle::sel;
+                break;
+            
+            
+            default:
+                break;
+            }
+
+        }
+
+        void ciclo_de_op () {
+
+            switch (sc)
+            {
+            case sinais_controle::selUAL:
+                //parte_operativa.rem.str();
+                sc = sinais_controle::sel;
+                break;
+            
+            case sinais_controle::cargaAC:
+                //parte_operativa.rem.str();
+                sc = sinais_controle::sel;
+                break;
+            
+            case sinais_controle::cargaNZ:
+                //parte_operativa.rem.str();
+                sc = sinais_controle::sel;
+                break;
+            
+            
+            default:
+                break;
+            }
+
+        }
+
+        void sinais_de_controle () {
+
+            ciclo_de_busca();
+            ciclo_de_op();
+
+        }
+
+
+
+
+    };
     bool N = 0;
     bool Z = 0;
-    states state;
-
-    public:
-
-
-    MEM memory;
-    REM rem;
-    PC program_counter;
-    ULA ula;
-
-    REG ra;
-
+    states state;    
+    
+    
+    PO parte_operativa;
+    UC uni_cont;
 
 
 
     std::string read(int idx) {
-        return memory.M[idx];
+        return parte_operativa.carga_mem.read(parte_operativa.memoria, idx);
     }
 
     void write(int idx, std::string content) {
-        memory.M[idx] = content;
+        parte_operativa.carga_mem.write(parte_operativa.memoria, content, idx);
     }
 
     void jump(int a) {
-        program_counter.p = a;
+        //program_counter.p = a;
+        parte_operativa.program_count.receive(a);
     }
 
     void jn(int end) {
         if (N == 1) {
-            program_counter.p = end;
+            //program_counter.p = end;
+            parte_operativa.program_count.receive(end);
         }
     }
 
     void jz(int end) {
         if (Z == 1) {
-            program_counter.p = end;
+            //program_counter.p = end;
+            parte_operativa.program_count.receive(end);
         }
     }
 
     void add(REG &r, std::string mem_end) {
         int b = std::stoi(mem_end);
 
-        ula.add(r, b);
+        parte_operativa.unidade_arit.add(r, b);
 
     }
 
     void sub(REG &r, std::string mem_end) {
         int b = std::stoi(mem_end);
 
-        ula.sub(r, b);
+         parte_operativa.unidade_arit.sub(r, b);
 
     }
 
     void andy(REG &r, std::string mem_end) {
         int b = std::stoi(mem_end);
 
-        ula.andy(r, b);
+        parte_operativa.unidade_arit.andy(r, b);
 
     }
 
     void ore(REG &r, std::string mem_end) {
         int b = std::stoi(mem_end);
 
-        ula.ore(r, b);
+        parte_operativa.unidade_arit.ore(r, b);
 
     }
 
     void note(REG &r, std::string mem_end) {
         int b = std::stoi(mem_end);
 
-        ula.note(r);
+        parte_operativa.unidade_arit.note(r);
 
     }
 
@@ -189,15 +343,19 @@ class Neander{
             }
 
             std::vector<std::string> a = tokenize(line);
-
-            memory.M[cont] = a[0];
+            
+            parte_operativa.memoria.write(a[0], cont);
+            //parte_operativa.memoria.M[cont] = a[0];
             cont++;
-            memory.M[cont] = a[2];
+            parte_operativa.memoria.write(a[2], cont);
+            //memory.M[cont] = a[2];
             cont++;
 
             int b = stoi(a[2]);
 
-            memory.M[b] = 250;
+            parte_operativa.memoria.write("250", b);
+
+            //memory.M[b] = 250;
 
             //processador.
 
@@ -257,18 +415,7 @@ int main(int argc, char* argv[]){
 
     // como a gente faria a parte de colocar na memória?
     // teríamos que codar no assembly:
-    while(std::getline(assembly, line)){
-        if(line.empty()){
-            continue;
-        }
-
-        std::vector<std::string> a = tokenize(line);
-
-        //processador.
-
-        
-        
-    }
+    processador.prepare(assembly, line);
     
     
 }
